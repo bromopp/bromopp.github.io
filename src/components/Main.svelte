78847ac1a4e9a82e8a35b3a967ca23eb1aab0b84
Bruno <bruno.oppizzi@gmail.com>
2023-11-06 19:47:28 -0300

    Svelt rewrite

---
+++ src/components/Main.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap 10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">Hi! I'm <span class="text-violet-400">Bruno </span>Oppizzi <br/> Web & Game <span class="text-violet-400">Developer </span></h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                                
+            </p>
+        </div>
+    </section>
+</main>
